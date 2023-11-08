3397ecb0b4b6d92a707e29046203b3a2d8455b46
Golam Kibria <uchihai850@gmail.com>
2023-11-08 22:42:57 +0600

    Vercel Deployment
    Added vercel.json config file and set up vercel as well as deployed the server to vercel. also added all the environment variables in vercel.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://elysium-5dc18.web.app',
-        'https://elysium-5dc18.firebaseapp.com/',
+        'https://elysium-5dc18.firebaseapp.com',
     ],
     credentials: true
 }))
@@ -60,7 +60,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
 
         const elysiumDB = client.db('elysiumDB');
         const foodItemsCollection = elysiumDB.collection('foodItems');
@@ -239,7 +239,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        //await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
